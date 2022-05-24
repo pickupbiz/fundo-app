795bf070021281c57cde4f4becd7168727de2afd
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-05-24 12:48:45 +0530

    Added Counter

--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Toggle } from "./Toggle";
+import { Counter } from "./Counter";
 
 export const Fun = () => {
   return (
     <div className="home">
       <Toggle />
+      <Counter />
     </div>
   );
 };
