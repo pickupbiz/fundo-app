d7e2f89a8adc8b187a91b422def0c9950880bbea
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2022-05-24 19:14:33 +0530

    play added

--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Toggle } from "./Toggle";
 import { Counter } from "./Counter";
+import { Player } from "./Player";
 
 export const Fun = () => {
   return (
     <div className="home">
       <Toggle />
       <Counter />
+      <Player />
     </div>
   );
 };
